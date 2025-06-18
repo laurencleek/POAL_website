baf8860f56e0c948316ea38c4438b53e2b800d32
laurencleek <lcleek@hotmail.com>
2025-06-18 14:20:26 +0200

    update People pages with websites and bios

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -8,21 +8,31 @@ export default function PeoplePage() {
       name: "Prof Will Jennings",
       title: "Co-director",
       image: "/bio_will.jpg?height=300&width=300&text=WJ",
-      bio: "Overseeing the impact and public engagement theme",
+      personal_website: "https://www.linkedin.com/today/author/will-jennings-91b01224",
+      institutional_website: "https://www.southampton.ac.uk/people/5x8zrf/professor-will-jennings",
+      role: "Overseeing the impact and public engagement theme",
+      focus: "Public Policy, Public Opinion, Polling",
     },
     {
       id: 2,
       name: "Prof Sara Hobolt",
       title: "Co-director",
       image: "/bio_hobolt.jpg?height=300&width=300&text=SH",
-      bio: "Overseeing the research excellence theme",
+      personal_website: "https://hobolt.com/",
+      institutional_website: "https://www.lse.ac.uk/government/people/academic-staff/sara-hobolt",
+      role: "Overseeing the research excellence theme",
+      focus: "Elections, Referendums, Public Opinion, European Union Politics",
     },
     {
       id: 3,
       name: "Dr Miriam Sorace",
       title: "Co-director",
       image: "/bio_miriam.png?height=300&width=300&text=MS",
-      bio: "Overseeing the innovation and training theme",
+      personal_website: "https://miriamsorace.github.io/",
+      institutional_website: "https://www.reading.ac.uk/politics-international-relations/staff/miriam-sorace",
+      role: "Overseeing the innovation and training theme",
+      focus: "Electoral Behaviour, Public Opinion; Political Campaigns; European Politics",
+  
     },
   ]
 
@@ -32,194 +42,204 @@ export default function PeoplePage() {
       name: "Prof Gabi Durrant",
       title: "Research Fellow",
       image: "/bio_gabi.png?height=200&width=200&text=GD",
-      focus: "bio to be added here",
+      personal_website: "https://www.southampton.ac.uk/people/5x25ff/professor-gabriele-durrant",
+      institutional_website: "https://www.southampton.ac.uk/people/5x25ff/professor-gabriele-durrant",
+      focus: "Survey Data Collection, Survey Methodology, Statistical Modelling"
     },
     {
       id: 5,
-      name: "Prof Ken Benoit",
+      name: "Prof Kenneth Benoit",
       title: "Research Fellow",
       image: "/bio_ken.jpg?height=200&width=200&text=KB",
-      focus: "bio to be added here",
+      personal_website: "https://kenbenoit.net/",
+      institutional_website: "https://socsc.smu.edu.sg/faculty/profile/7891/kenneth-benoit",
+      focus: "Computational social science, text analysis, social media, LLMs"
+     
     },
     {
       id: 6,
       name: "Prof Jason Reifler",
       title: "Research Fellow",
       image: "/bio_jason.jpg?height=200&width=200&text=JR",
-      focus: "bio to be added here",
+      personal_website: "https://jasonreifler.com/",
+      institutional_website: "https://www.southampton.ac.uk/people/65dh7n/professor-jason-reifler",
+      focus: "Public Opinion, Political Behavior, and Misinformation"
     },
     {
       id: 7,
       name: "Prof Patrick Sturgis",
       title: "Research Fellow",
       image: "/bio_patrick.png?height=200&width=200&text=PS",
-      focus: "bio to be added here",
+      personal_website: "https://www.lse.ac.uk/Methodology/People/Academic-Staff/Patrick-Sturgis/Patrick-Sturgis",
+      institutional_website: "https://www.lse.ac.uk/Methodology/People/Academic-Staff/Patrick-Sturgis/Patrick-Sturgis",
+      focus: "Survey and Statistical Methods, Understanding Social and Political Behaviour"
     },
     {
       id: 8,
       name: "Prof Tereza Capelos",
       title: "Research Fellow",
       image: "/bio_tereza.jpg?height=200&width=200&text=TC",
-      focus: "bio to be added here",
+      personal_website: "https://www.southampton.ac.uk/people/656nwf/professor-tereza-capelos",
+      institutional_website: "https://www.southampton.ac.uk/people/656nwf/professor-tereza-capelos",
+      focus: "Grievance Politics, Resentful Emotionality, Reactionary Orientations, Anti-democratic and Authoritarian Political Preferences"
+,
     },
     {
       id: 9,
       name: "Prof Robert Johns",
       title: "Research Fellow",
       image: "/bio_robert.jpg?height=200&width=200&text=RJ",
-      focus: "bio to be added here",
+      personal_website: "https://www.southampton.ac.uk/people/65brpg/professor-robert-johns",
+      institutional_website: "https://www.southampton.ac.uk/people/65brpg/professor-robert-johns",
+      focus: "Public Opinion, British and especially Scottish elections, Political Psychology"
+     
     },
     {
       id: 10,
       name: "Dr Katharina Lawall",
       title: "Research Fellow",
       image: "/bio_katharina.png?height=200&width=200&text=KL",
-      focus: "bio to be added here",
+      personal_website: "https://katharinalawall.com/",
+      institutional_website: "https://www.reading.ac.uk/politics-international-relations/staff/dr-katharina-lawall",
+      focus: "Political Behaviour, Gender and Politics, Polarization, Emotions, Radical Right, Experiments"
+     
     },
     {
       id: 11,
       name: "Dr Victor Araujo Silva",
       title: "Research Fellow",
       image: "/bio_victor.jpg?height=200&width=200&text=VA",
-      focus: "bio to be added here",
+      personal_website: "https://sites.google.com/view/victoraraujo/about-me",
+      institutional_website: "https://www.reading.ac.uk/politics-international-relations/staff/dr-victor-araujo",
+      focus: "The Americas (US and LA), Political Polarization, Populism, Persuasion, Identity Politics, Survey methodology and field experiments"
+,
     },
     {
       id: 12,
       name: "Dr Matthias Kroenke",
       title: "Research Fellow",
       image: "/bio_matthias.jpg?height=200&width=200&text=MK",
-      focus: "bio to be added here",
+      personal_website: "https://www.matthiaskroenke.com",
+      institutional_website: "https://www.reading.ac.uk/politics-international-relations/staff/dr-matthias-kroenke ",
+      focus: "Comparative Politics, Political Behavior, Sub-Saharan Africa, Quantitative and Qualitative Methods,"
+,
     },
     {
       id: 13,
       name: "Dr Thomas Robinson",
       title: "Research Fellow",
       image: "/bio_tom.jpg?height=200&width=200&text=TR",
-      focus: "bio to be added here",
+      personal_website: "https://ts-robinson.com",
+      institutional_website: "https://www.lse.ac.uk/Methodology/People/Academic-Staff/Thomas-Robinson/",
+      focus: "Money in Politics, Voting Behaviour, and Methodological issues in experimental design and machine learning"
     },
     {
       id: 14,
       name: "Dr Ben Tappin",
       title: "Research Fellow",
       image: "/bio_ben.png?height=200&width=200&text=BT",
-      focus: "bio to be added here",
+      personal_website: "https://www.benmtappin.com/",
+      institutional_website: "https://www.lse.ac.uk/pbs/people/Dr-Ben-Tappin",
+      focus: "Technology, Persuasive Communication, and Attitude and Behaviour Change"
+
     },
     {
       id: 15,
       name: "Dr Florian Foos",
       title: "Research Fellow",
       image: "/bio_florian.png?height=200&width=200&text=FF",
-      focus: "bio to be added here",
+      personal_website: "http://www.florianfoos.net/",
+      institutional_website: "https://www.lse.ac.uk/government/people/academic-staff/florian-foos",
+      focus: "Randomized Experiments, Field Experiments, Election campaigns, Social Media"
     },
     {
       id: 16,
       name: "Dr Zach Dickson",
       title: "Research Fellow",
       image: "/bio_zac.jpg?height=200&width=200&text=ZD",
-      focus: "bio to be added here",
+      personal_website: "https://z-dickson.github.io",
+      institutional_website: "https://www.lse.ac.uk/Methodology/People/Academic-Staff/Zach-Dickson/Zach-Dickson",
+      focus: "US and UK politics, Elites, Representation and Public Opinion using Causal Inference and Computational Methods",
+
     },
     {
       id: 17,
       name: "Dr Stuart Turnbull-Dugarte",
       title: "Research Fellow",
       image: "/bio_stuart.jpg?height=200&width=200&text=ST",
-      focus: "bio to be added here",
+      personal_website: "https://turnbulldugarte.com/",
+      institutional_website: "https://www.southampton.ac.uk/people/5y5zj6/doctor-stuart-turnbull-dugarte",
+      focus: "experimental methods, political sociology, LGBTQ+ politics"
     },
     {
       id: 18,
       name: "Dr Daniel Devine",
       title: "Research Fellow",
       image: "/bio_daniel.jpg?height=200&width=200&text=DD",
-      focus: "bio to be added here",
+      personal_website: "https://djdevine.org/",
+      institutional_website: "https://www.southampton.ac.uk/people/65grkh/doctor-daniel-devine",
+      focus: "Public Opinion, Voting Behaviour, Political Psychology"
     },
     {
       id: 19,
       name: "Dr Jessica Smith",
       title: "Research Fellow",
       image: "/bio_jessica.jpg?height=200&width=200&text=JS",
-      focus: "bio to be added here",
+      personal_website: "https://drjessicacsmith.wordpress.com",
+      institutional_website: "https://www.southampton.ac.uk/people/5xt882/doctor-jessica-smith",
+      focus: "Political behaviour and gender-based stereotyping, Executive Politics, British Politics",
+
     },
     {
       id: 20,
       name: "Dr Vanessa Cheng-Matsuno",
       title: "Research Fellow",
       image: "/bio_vanessa.jpg?height=200&width=200&text=VC",
-      focus: "bio to be added here",
+      personal_website: "https://vchengm.wordpress.com/",
+      institutional_website: "https://www.lse.ac.uk/government/people/research-students/vanessa-cheng-matsuno",
+      focus: "Political Economy, Political behaviour, Quantitative methods, Data science"
     },
     {
       id: 21,
       name: "Dr Olga Maslovskaya",
       title: "Research Fellow",
       image: "/bio_olga.jpg?height=200&width=200&text=OM",
-      focus: "bio to be added here",
+      personal_website: "https://www.southampton.ac.uk/people/5wzzpw/doctor-olga-maslovskaya",
+      institutional_website: "https://www.southampton.ac.uk/people/5wzzpw/doctor-olga-maslovskaya",
+      focus: "All Aspects of Survey, Survey Data Colleciton, Data Quality"
     },
     {
       id: 22,
       name: "Dr Marta Antonetti",
       title: "Research Fellow",
       image: "/bio_anton.jpeg?height=200&width=200&text=MA",
-      focus: "bio to be added here",
+      personal_website: "https://www.martaantonetti.com",
+      institutional_website: "https://www.lse.ac.uk/government/people",
+      focus: "Democratic Values, Political Participation, Political leaders and Campaigns, Experimental Methods, Survey Designs ",
+
     },
     {
       id: 23,
       name: "Lauren Leek",
       title: "Research Fellow",
       image: "/bio_Leek.png?height=200&width=200&text=LL",
-      focus: "bio to be added here",
+      personal_website: "https://laurenleek.eu/",
+      institutional_website: "https://www.eui.eu/people?id=lauren-leek",
+      focus: "Political Economy, Public Policy, Public Opinion using Causal Inference and Computational Methods",
+
     },
     {
       id: 24,
       name: "Conrad Martinez-Lambert",
       title: "Research Fellow",
       image: "/bio_conrad.jpg?height=200&width=200&text=LL",
-      focus: "bio to be added here",
+      personal_website: "https://uk.linkedin.com/in/conrad-pollcat-elections-postgrad",
+      institutional_website: "https://uk.linkedin.com/in/conrad-pollcat-elections-postgrad",
+      focus: "Inequality and Polarization, Public Opinion, Elections",
     },
   ]
 
-  const openPositions = [
-    {
-      id: "eoi1",
-      name: "[EoIs open]",
-      title: "Academic events lead",
-      image: "/placeholder.svg?height=200&width=200&text=EOI",
-      focus: "Seminar series, writing retreats, conferences",
-    },
-    {
-      id: "eoi2", 
-      name: "[EoIs open]",
-      title: "Grant unit lead",
-      image: "/placeholder.svg?height=200&width=200&text=EOI",
-      focus: "bio to be added here",
-    },
-    {
-      id: "eoi3",
-      name: "[EoIs open]",
-      title: "Survey methodology unit lead",
-      image: "/placeholder.svg?height=200&width=200&text=EOI",
-      focus: "bio to be added here",
-    },
-    {
-      id: "eoi4",
-      name: "[EoIs open]",
-      title: "Executive and academic programmes lead",
-      image: "/placeholder.svg?height=200&width=200&text=EOI",
-      focus: "bio to be added here",
-    },
-    {
-      id: "eoi5",
-      name: "[EoIs open]",
-      title: "Media unit lead",
-      image: "/placeholder.svg?height=200&width=200&text=EOI",
-      focus: "bio to be added here",
-    },
-    {
-      id: "eoi6",
-      name: "[EoIs open]",
-      title: "Contracts/consultancy unit lead",
-      image: "/placeholder.svg?height=200&width=200&text=EOI",
-      focus: "bio to be added here",
-    },
-  ]
+  
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -247,10 +267,36 @@ export default function PeoplePage() {
                 <div className="p-6">
                   <h3 className="text-xl font-bold mb-1">{person.name}</h3>
                   <p className="text-emerald-600 mb-4">{person.title}</p>
-                  <p className="text-gray-700 mb-4">{person.bio}</p>
-                  <Link href={`/people/${person.id}`} className="text-emerald-600 hover:text-emerald-800 font-medium">
-                    Full profile
-                  </Link>
+                  <p className="text-gray-700 mb-4">{person.bio || person.role}</p>
+                  <div className="flex items-center space-x-4 mb-4 text-sm">
+                    {"personal_website" in person && (
+                      <Link
+                        href={person.personal_website}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-emerald-600 hover:text-emerald-800 font-medium"
+                      >
+                        Personal
+                      </Link>
+                    )}
+                    {"institutional_website" in person && (
+                      <Link
+                        href={person.institutional_website}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-emerald-600 hover:text-emerald-800 font-medium"
+                      >
+                        Institutional
+                      </Link>
+                    )}
+                  </div>
+                  {"focus" in person ? (
+                    <p className="text-gray-700">{person.focus}</p>
+                  ) : (
+                    <Link href={`/people/${person.id}`} className="text-emerald-600 hover:text-emerald-800 font-medium">
+                      Full profile
+                    </Link>
+                  )}
                 </div>
               </div>
             ))}
@@ -272,28 +318,36 @@ export default function PeoplePage() {
                 </div>
                 <h3 className="text-lg font-bold">{person.name}</h3>
                 <p className="text-emerald-600 mb-2">{person.title}</p>
-                <p className="text-gray-600 text-sm">{person.focus}</p>
+                <p className="text-gray-600 text-sm mb-2">{person.focus}</p>
+                <div className="flex items-center space-x-4 mt-auto pt-2 text-sm">
+                  {"personal_website" in person && (
+                    <Link
+                      href={person.personal_website}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-emerald-600 hover:text-emerald-800 font-medium"
+                    >
+                      Personal
+                    </Link>
+                  )}
+                  {"institutional_website" in person && (
+                    <Link
+                      href={person.institutional_website}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-emerald-600 hover:text-emerald-800 font-medium"
+                    >
+                      Institutional
+                    </Link>
+                  )}
+                </div>
               </div>
             ))}
           </div>
         </div>
       </section>
 
-      <section className="py-16 px-4 md:px-6 bg-gray-100">
-        <div className="container mx-auto text-center">
-          <h2 className="text-2xl font-bold mb-8">Join Our Team</h2>
-          <p className="max-w-2xl mx-auto text-gray-700 mb-8">
-            We're always looking for talented researchers, data scientists, and policy experts who are passionate about
-            improving economic opportunity.
-          </p>
-          <Link
-            href="/careers"
-            className="inline-block px-6 py-3 bg-emerald-600 text-white font-medium rounded-md hover:bg-emerald-700 transition-colors"
-          >
-            View Open Positions
-          </Link>
-        </div>
-      </section>
+     
     </div>
   )
 }
